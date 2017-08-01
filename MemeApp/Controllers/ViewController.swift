7aae425e356d83b11175534b305e1f3ac85eff2f
Lukeghenco <lukeghenco@gmail.com>
2017-08-01 14:58:09 -0400

    Add Meme with ability to save

---
+++ MemeApp/Controllers/ViewController.swift
@@ -0,0 +1,146 @@
+//
+//  ViewController.swift
+//  MemeApp
+//
+//  Created by Luke  on 7/31/17.
+//  Copyright © 2017 Luke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var imagePickerView: UIImageView!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
+    
+    // MARK: - Properties
+    let defaultText = "Click To Edit Text"
+    
+    // MARK: - Lifecycle Hooks
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.topTextField.delegate = self
+        self.bottomTextField.delegate = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+        setTextFieldAttributes()
+        self.subscribeToKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        self.unsubscribeToKeyboardNotifications()
+    }
+    
+    // MARK: - TextField Functions
+    func setTextFieldAttributes() {
+        let memeTextAttributes: [String:Any] = [
+            NSAttributedStringKey.strokeColor.rawValue: UIColor.black,
+            NSAttributedStringKey.foregroundColor.rawValue: UIColor.white,
+            NSAttributedStringKey.font.rawValue: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+            NSAttributedStringKey.strokeWidth.rawValue: 3.0
+        ]
+        
+        self.topTextField.text = defaultText
+        self.topTextField.defaultTextAttributes = memeTextAttributes
+        
+        self.bottomTextField.text = defaultText
+        self.bottomTextField.defaultTextAttributes = memeTextAttributes
+    }
+    
+    // MARK: - Save Meme
+    func save() {
+        let meme = Meme(topText: topTextField.text, bottomText: bottomTextField.text, originalImage: imagePickerView.image!, memedImage: generateMemedImage())
+    }
+    
+    func generateMemedImage() -> UIImage {
+        
+        // TODO: Hide Toolbar and Navbar
+        self.navigationController?.toolbar.isHidden = true
+        self.navigationController?.navigationBar.isHidden = true
+        
+        // Render View to an Image
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        view.drawHierarchy(in: self.view.frame, afterScreenUpdates: true)
+        let memedImage: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        
+        // TODO: Show Toolbar and Navbar
+        self.navigationController?.toolbar.isHidden = false
+        self.navigationController?.navigationBar.isHidden = false
+        
+        return memedImage
+    }
+    
+    // MARK: - ImagePicker Delegate Functions
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let image = info["UIImagePickerControllerOriginalImage"] as? UIImage {
+            print(image)
+            self.imagePickerView.image = image
+            picker.dismiss(animated: true, completion: nil)
+        }
+    }
+    
+    // MARK: - TextFieldDelegate Functions
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField.text == defaultText {
+            textField.text = ""
+            textField.textAlignment = .center
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return true
+    }
+    
+    // MARK: - Keyboard Functions
+    @objc func keyboardWillShow(notification: NSNotification) {
+        self.view.frame.origin.y -= getKeyboardHeight(notification: notification)
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        self.view.frame.origin.y = 0
+    }
+    
+    func getKeyboardHeight(notification: NSNotification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue
+        
+        return keyboardSize.cgRectValue.height
+    }
+    
+    func subscribeToKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: .UIKeyboardWillHide, object: nil)
+    }
+    
+    func unsubscribeToKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillHide, object: nil)
+    }
+    
+    // MARK: - IBActions
+    @IBAction func pickAnImageFromAlbum(_ sender: Any) {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        imagePickerController.sourceType = .photoLibrary
+        self.present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    @IBAction func pickAnImageFromCamera(_ sender: UIBarButtonItem) {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        imagePickerController.sourceType = .camera
+        self.present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    
+}
+
---
+++ MemeApp/Models/Meme.swift
@@ -0,0 +1,17 @@
+//
+//  Meme.swift
+//  MemeApp
+//
+//  Created by Luke  on 8/1/17.
+//  Copyright © 2017 Luke. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Meme {
+    var topText: String?
+    var bottomText: String?
+    var originalImage: UIImage
+    var memedImage: UIImage
+}
