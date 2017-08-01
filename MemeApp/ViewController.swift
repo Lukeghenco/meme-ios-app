2c564fa0dd7aa1b5773f1f0d650bc97ac6fec414
Lukeghenco <lukeghenco@gmail.com>
2017-08-01 07:09:04 -0400

    Add image view and toolbar

--- MemeApp/ViewController.swift
+++ MemeApp/ViewController.swift
@@ -8,18 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var imagePickerView: UIImageView!
+    
+    // MARK: - Lifecycle Hooks
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // MARK: - ImagePicker Delegate Functions
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let image = info["UIImagePickerControllerOriginalImage"] as? UIImage {
+            print(image)
+            self.imagePickerView.image = image
+            picker.dismiss(animated: true, completion: nil)
+        }
     }
-
-
+    
+    // MARK: - IBActions
+    @IBAction func pickAnImageFromAlbum(_ sender: Any) {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        self.present(imagePickerController, animated: true, completion: nil)
+    }
 }
 
